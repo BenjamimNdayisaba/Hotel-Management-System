374159c476a4706ce523e40e585c87213599e1ee
本杰明 <156743796+BenjamimNdayisaba@users.noreply.github.com>
2025-06-04 03:09:56 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+config = {
+    "DB_HOST": "localhost",
+    "DB_USER": "root",
+    "DB_PASSWORD": "Ben1234@",
+    "DB_NAME": "hms",
+    "DB_PORT": 3306,
+    "DB_AUTOCOMMIT": True,
+}
